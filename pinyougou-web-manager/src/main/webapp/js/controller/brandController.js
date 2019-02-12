9052ae35293f18aa5b3e4e69cb291d855080bb8a
Administrator <1471116323@qq.com>
2019-02-12 17:37:04 +0800

    第一次修改运营商后台品牌管理页面,添加状态,修改brand pojo

--- pinyougou-web-manager/src/main/webapp/js/controller/brandController.js
+++ pinyougou-web-manager/src/main/webapp/js/controller/brandController.js
@@ -79,5 +79,19 @@ app.controller("brandController",function($scope,$controller,brandService){
 			$scope.list = response.rows;
 		});
 	}
+
+    // 显示状态
+    $scope.status = ["未审核","审核通过","审核未通过","关闭"];
+    // 审核的方法:
+    $scope.updateStatus = function(status){
+        goodsService.updateStatus($scope.selectIds,status).success(function(response){
+            if(response.flag){
+                $scope.reloadList();//刷新列表
+                $scope.selectIds = [];
+            }else{
+                alert(response.message);
+            }
+        });
+    }
 	
 });
