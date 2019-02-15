7a9d6f68e770c96d1bfa7f1f55b8b3646920a8a8
yuyan199859 <948709473@qq.com>
2019-02-15 18:20:51 +0800

    加了些东西。 增删改收货人地址。 一样的

--- pinyougou-web-portal/src/main/java/cn/itcast/core/controller/address/AddressController.java
+++ pinyougou-web-portal/src/main/java/cn/itcast/core/controller/address/AddressController.java
@@ -1,9 +1,13 @@
 package cn.itcast.core.controller.address;
 
+
+import cn.itcast.core.entity.Result;
 import cn.itcast.core.pojo.address.Address;
+import cn.itcast.core.pojo.good.Brand;
 import cn.itcast.core.service.address.AddressService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +22,42 @@ public class AddressController {
 
     /**
      * 加载当前收货人的地址列表
+     *
      * @return
      */
     @RequestMapping("/findListByLoginUser.do")
-    public List<Address> findListByLoginUser(){
+    public List<Address> findListByLoginUser() {
         String userId = SecurityContextHolder.getContext().getAuthentication().getName();
         return addressService.findListByLoginUser(userId);
+    }
+    @RequestMapping("/add.do")
+    public Result add(@RequestBody Address address){
+        try {
+           addressService.add(address);
+            return new Result(true, "保存成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "保存失败");
+        }
+    }
+    @RequestMapping("/delete.do")
+    public Result delete(Long[] ids){
+        try{
+            addressService.delete(ids);
+            return new Result(true, "删除成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,"删除失败");
+        }
+    }
+    @RequestMapping("/update.do")
+    public Result update(@RequestBody Address address){
+        try {
+            addressService.update(address);
+            return new Result(true, "更新成功");
+        }catch(Exception e){
+            e.printStackTrace();
+            return new Result(false, "更新失败");
+        }
     }
 }
