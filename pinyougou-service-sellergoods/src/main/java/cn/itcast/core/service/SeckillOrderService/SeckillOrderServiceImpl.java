ef580b0b8db739db39c447d91a79fe7e4b55b225
jibaba <ji@332.com>
2019-02-15 14:27:36 +0800

    优化和添加和秒杀商品申请方法实现

--- pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/SeckillOrderService/SeckillOrderServiceImpl.java
+++ pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/SeckillOrderService/SeckillOrderServiceImpl.java
@@ -5,6 +5,7 @@ import cn.itcast.core.dao.seckill.SeckillOrderDao;
 import cn.itcast.core.entity.PageResult;
 import cn.itcast.core.pojo.good.Brand;
 import cn.itcast.core.pojo.seckill.SeckillOrder;
+import cn.itcast.core.pojo.seckill.SeckillOrderQuery;
 import cn.itcast.core.service.seckillOrder.SeckillOrderService;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
@@ -24,6 +25,17 @@ public class SeckillOrderServiceImpl implements SeckillOrderService {
         // 1、设置分页条件-分页插件
         PageHelper.startPage(page, rows);
         // 2、根据条件查询
+        SeckillOrderQuery seckillOrderQuery = new SeckillOrderQuery();
+        seckillOrderQuery.createCriteria().andSellerIdEqualTo(seckillOrder.getSellerId());
+        Page<SeckillOrder> resul = (Page<SeckillOrder>) seckillOrderDao.selectByExample(seckillOrderQuery);
+        // 3、创建PageResult对象并且封装结果
+        return new PageResult(resul.getResult(), resul.getTotal());
+    }
+
+    @Override
+    public PageResult searchForShop1(Integer page, Integer rows, SeckillOrder seckillOrder) {
+        PageHelper.startPage(page, rows);
+
         Page<SeckillOrder> resul = (Page<SeckillOrder>) seckillOrderDao.selectByExample(null);
         // 3、创建PageResult对象并且封装结果
         return new PageResult(resul.getResult(), resul.getTotal());
--- pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/seckill/SeckillServiceImpl.java
+++ pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/seckill/SeckillServiceImpl.java
@@ -4,12 +4,16 @@ import cn.itcast.core.dao.seckill.SeckillGoodsDao;
 import cn.itcast.core.entity.PageResult;
 import cn.itcast.core.pojo.seckill.SeckillGoods;
 import cn.itcast.core.pojo.seckill.SeckillGoodsQuery;
+import cn.itcast.core.vo.GoodsVo;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 class SeckillServiceImpl implements SeckillService {
@@ -23,11 +27,11 @@ class SeckillServiceImpl implements SeckillService {
         // 设置查询条件：待审核并且是未删除
         SeckillGoodsQuery seckillGoodsQuery = new SeckillGoodsQuery();
         SeckillGoodsQuery.Criteria criteria = seckillGoodsQuery.createCriteria();
-        if (seckillGoods.getStatus() != null && !"".equals(seckillGoods.getStatus().trim())) {
-            criteria.andStatusEqualTo(seckillGoods.getStatus().trim());
+        if (seckillGoods.getTitle() != null && !"".equals(seckillGoods.getTitle() .trim())) {
+            criteria.andStatusEqualTo(seckillGoods.getTitle() .trim());
         }
+        criteria.andSellerIdEqualTo(seckillGoods.getSellerId());
         criteria.andStatusEqualTo("0");
-        seckillGoodsQuery.setOrderByClause("id desc");
         // 根据条件查询
         Page<SeckillGoods> p = (Page<SeckillGoods>) seckillGoodsDao.selectByExample(seckillGoodsQuery);
         // 封装结果
@@ -40,4 +44,17 @@ class SeckillServiceImpl implements SeckillService {
         Page<SeckillGoods> seckillGoods = (Page<SeckillGoods>) seckillGoodsDao.selectByExample(null);
         return  new PageResult(seckillGoods.getResult(),seckillGoods.getTotal());
     }
+
+    @Transactional
+    @Override
+    public void add(SeckillGoods seckillGoods) {
+        seckillGoods.setCreateTime(new Date());
+        seckillGoods.setStartTime(new Date());
+        seckillGoods.setEndTime(new Date());
+        seckillGoods.setStatus("0");
+        seckillGoodsDao.insertSelective(seckillGoods);
+
+    }
+
+
 }
