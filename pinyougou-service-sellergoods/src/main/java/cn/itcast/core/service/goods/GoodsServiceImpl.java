6e726656240bda1804a529d909f8d5c3375fa493
yyz <yyz163.com>
2019-02-16 09:50:53 +0800

    在web-shop中创建orderController,创建方法updateOrderStatus 2.测试提交2

--- pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/goods/GoodsServiceImpl.java
+++ pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/goods/GoodsServiceImpl.java
@@ -394,7 +394,7 @@ public class GoodsServiceImpl implements GoodsService {
      * @param username
      * @return
      */
-    //测试
+    //测试2
     @Override
     public List<Long> findcollectGoodsFromRedis(String username) {
         return (List<Long>) redisTemplate.boundHashOps("Collection").get(username);
