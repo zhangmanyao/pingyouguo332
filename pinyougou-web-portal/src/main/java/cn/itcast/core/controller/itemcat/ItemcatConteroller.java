37f1e102cc33e7d6f2ad252f22c3373e84cd8a91
zhangyaoayo <252523914@qq.com>
2019-02-16 09:30:04 +0800

    前台商品分类的实现

--- pinyougou-web-portal/src/main/java/cn/itcast/core/controller/itemcat/ItemcatConteroller.java
+++ pinyougou-web-portal/src/main/java/cn/itcast/core/controller/itemcat/ItemcatConteroller.java
@@ -1,7 +1,7 @@
-package cn.itcast.core.controller.item_cat;
+package cn.itcast.core.controller.itemcat;
 
 import cn.itcast.core.pojo.item.ItemCat;
-import cn.itcast.core.service.itemcat.ItemCatService;
+import cn.itcast.core.service.itemcat.ItemCatListService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,20 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/itemCat")
-public class ItemCatConteroller {
+@RequestMapping(value = "itemCat")
+public class ItemcatConteroller {
 
     @Reference
-    private ItemCatService itemCatService;
+    private ItemCatListService itemCatListService;
 
     /**
      * 查询商品分类信息
      *
      * @return
      */
-    @RequestMapping("/findItemCatList.do")
+    @RequestMapping(value = "findItemCatList.do")
     public List<ItemCat> findItemCatList() {
-        List<ItemCat> itemCatList = itemCatService.findItemCatList();
+        List<ItemCat> itemCatList = itemCatListService.findItemCatList();
         return itemCatList;
     }
+
 }
