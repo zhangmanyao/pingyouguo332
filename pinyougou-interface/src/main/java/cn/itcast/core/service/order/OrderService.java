ae2e9506924b0732490756e565131cd9167c6e4b
byy1072322827 <1072322827@qq.com>
2019-02-16 09:20:39 +0800

    接口。。。

--- pinyougou-interface/src/main/java/cn/itcast/core/service/order/OrderService.java
+++ pinyougou-interface/src/main/java/cn/itcast/core/service/order/OrderService.java
@@ -1,6 +1,9 @@
 package cn.itcast.core.service.order;
 
+import cn.itcast.core.entity.PageResult;
 import cn.itcast.core.pojo.order.Order;
+
+import java.util.Date;
 
 public interface OrderService {
 
@@ -10,4 +13,26 @@ public interface OrderService {
      * @param order
      */
     public void add(String username, Order order);
+
+    /**
+     * 待审核的商家的统计
+     * @param page
+     * @param rows
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+    public PageResult statistics(Integer page, Integer rows, Date startDate,Date endDate,Order order);
+
+    /**
+     * 运营商系统订单查询
+     * @param page
+     * @param rows
+     * @param minPrice
+     * @param maxPrice
+     * @param order
+     * @return
+     */
+    PageResult search(Integer page, Integer rows, String minPrice, String maxPrice, Order order);
+
 }
